0a4ebe16f8837e297d2e88807e8ab6489b987da3
GsaiberS-Kotahos <soborovets@gmail.com>
2026-04-27 13:19:05 +0700

    123

--- index.php
+++ index.php
@@ -4,6 +4,31 @@ require_once __DIR__ . '/src/database.php';
 require_once __DIR__ . '/src/Models/Record.php';
 require_once __DIR__ . '/src/Controllers/RecordController.php';
 require_once __DIR__ . '/src/Views/RecordView.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Crud\Controllers\RecordController;
+
+// Настройка соединения с БД через PDO [[14]]
+$dsn = 'mysql:host=localhost;dbname=example1;charset=utf8mb4';
+$username = 'root';
+$password = ''; // По умолчанию в XAMPP пароль пустой
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+
+    // Обработка GET-запроса
+    $controller = new RecordController($pdo);
+    $controller->index();
+
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo 'Ошибка подключения к БД: ' . htmlspecialchars($e->getMessage());
+}
 
 $controller = new RecordController();
 $view = new RecordView();
